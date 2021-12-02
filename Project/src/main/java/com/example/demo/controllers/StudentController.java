cd791e81691861228723f323ec3b5197b852d3c7
Gaurav-Dogra-coder <52917549+Gaurav-Dogra-coder@users.noreply.github.com>
2021-12-02 23:59:26 +0530

    read for given id, update and delete for id

--- Project/src/main/java/com/example/demo/controllers/StudentController.java
+++ Project/src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,21 +1,26 @@
 package com.example.demo.controllers;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
 import com.example.demo.repositories.StudentRepository;
-import java.util.*;
 @RestController
 public class StudentController {
 	@Autowired
 	public StudentRepository studentRepository;
 	
-	@GetMapping(value= "/all")
+	@GetMapping(value= "/read")
 	public List<Student> getAllStudents(){
 		try {
 		return studentRepository.findAll();
@@ -26,8 +31,17 @@ public class StudentController {
 			return l;
 		}
 	}
+	@GetMapping("/read/{id}")
+	public Student read(@PathVariable Long id) {
+		Optional<Student> Obj = studentRepository.findById(id);
+		if(Obj.isPresent()) {
+			return Obj.get();
+		}else {
+			throw new RuntimeException("Employee not found with id "+id);
+		}
+	}
 	
-	@GetMapping(value="/create")
+	@PostMapping(value="/create")
 	public String createStudent(@RequestBody Student student){	
 		
 		try {
@@ -36,6 +50,21 @@ public class StudentController {
 		}
 		catch(Exception e) {
 			return "eRROR";
+		}
+	}
+	@PutMapping("/update")
+	public Student update(@RequestBody Student modifiedObject) {
+		return studentRepository.save(modifiedObject);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		Optional<Student> studentobj = studentRepository.findById(id);
+		if(studentobj.isPresent()) {
+			studentRepository.delete(studentobj.get());
+			return "Employee deleted with id "+id;
+		}else {
+			throw new RuntimeException("Employee not found for id "+id);
 		}
 	}
 }
--- Project/src/main/java/com/example/demo/repositories/StudentRepository.java
+++ Project/src/main/java/com/example/demo/repositories/StudentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Student;
 @Repository
 public interface StudentRepository extends MongoRepository<Student,Long	> {
-
+	
 }
